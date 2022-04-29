6210dd7eeb8abedb250409d86fd01acfb90da9f9
Chad Rhiger <csrhig@gmail.com>
2022-04-29 09:23:54 -0500

    database.sql filled out.

--- database.sql
+++ database.sql
@@ -1,2 +1,15 @@
 -- Don't forget to add your create table SQL 
 -- It is also helpful to include some test data
+
+CREATE TABLE list (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(25) NOT NULL,
+	quantity numeric(10, 2) NOT NULL, 
+	unit VARCHAR(25) NOT NULL,
+  purchased boolean DEFAULT FALSE
+	);
+	
+SELECT * FROM list;
+
+INSERT INTO list (name, quantity, unit)
+VALUES ('milk', 1, 'gallon');
