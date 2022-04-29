0bbfae7eb3ce4e9bb014f4d865c7d5e65871c5f9
Madeline Prickette <madelineprickette@gmail.com>
2022-04-29 13:38:35 -0500

    got one half of get working

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const pool = require('../modules/pool.js');
 
 // TODO - Add routes here...
-
+router.get('/', (req, res) => {
+    console.log('GET /list');
+    res.sendStatus(200);
+})
 
 module.exports = router;
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
-
+import {useState, useEffect } from 'react';
+import axios from 'axios';
 import Header from '../Header/Header.jsx'
+import List from '../List/List.jsx'
 import './App.css';
 
 
 function App() {
+
+    useEffect(() => {
+        getShoppingList();
+    }, [])
+
+    const getShoppingList = () => {
+        axios({
+            method: 'GET',
+            url: '/list'
+        }).then ( (response) => {
+            console.log('get request is working');
+        }).catch ( (err) => {
+            console.log('something wrong with get app.jsx', err);
+        })
+    }
+
+const [shoppingList, setShoppingList] = useState([]);
+
     return (
         <div className="App">
             <Header />
